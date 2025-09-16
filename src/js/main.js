8c8652aca834a073c324ea8a01c5179a8b0dd2f1
Touseed Ahmed <touseefahmed1214@gmail.com>
2025-09-16 16:37:16 +0500

    paralex Effect Add

--- src/js/main.js
+++ src/js/main.js
@@ -105,3 +105,19 @@ function animateCycle() {
 }
 
 animateCycle();
+//paralex Effect
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".panel").forEach((panel, i) => {
+    ScrollTrigger.create({
+        trigger: panel,
+        start: "top top",
+        pin: true,
+        pinSpacing: false
+    });
+});
+
+
+ScrollTrigger.create({
+    snap: 1 / 4 // snap whole page to the closest section!
+});
